3f7a20cad95a4af45b5943ea1f408ecffd99bba5
JesusProyect <jesusno.ven@gmail.com>
2022-11-24 15:18:12 +0100

    app de heroes primer commit probando el lazy loading

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AuthRoutingModule } from './auth-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
