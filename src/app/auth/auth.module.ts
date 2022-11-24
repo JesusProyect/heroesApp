c4bd3cf45e9ed89ffbab1abcac65637d1e2e09af
JesusProyect <jesusno.ven@gmail.com>
2022-11-24 17:30:19 +0100

    terminada seccion de rutas y rutas hijas

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { CommonModule } from '@angular/common';
 
 
 
@@ -11,6 +12,7 @@ import { AuthRoutingModule } from './auth-routing.module';
     RegisterComponent
   ],
   imports: [
+    CommonModule,
     AuthRoutingModule
   ]
 })
