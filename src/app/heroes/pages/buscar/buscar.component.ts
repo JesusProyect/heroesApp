74aaed9a8b7e5f8286b559087e4e8070d12a2534
JesusProyect <jesusno.ven@gmail.com>
2022-11-29 16:33:16 +0100

    terminada seccion 13 empezamos la 14

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-buscar',
@@ -8,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  heroes: Heroe[] = [];
+  heroeSeleccionado: Heroe | undefined;
+  //sinResultados: boolean = false;
+  //guardaTermino: string = '';
+
+  constructor( private heroesServerice: HeroesService) { }
 
   ngOnInit(): void {
   }
+  buscando( ){
+    this.heroesServerice.getSugerencia( this.termino.trim() )
+      .subscribe( heroes => this.heroes = heroes);
+  }
+
+  opcionSeleccionada( event: MatAutocompleteSelectedEvent ) {
+   
+    if( !event.option.value ){
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+      const heroe: Heroe = event.option.value;
+      this.termino = heroe.superhero;
+
+      this.heroesServerice.getHeroesPorId( heroe.id! )
+       .subscribe( heroe => this.heroeSeleccionado = heroe );
+    
+  }
+
+
+ /*
+ ESTA FUE MI SOLUCION VOY A DEJAR LA QUE EL HIZO
+ buscando( ){
+    this.heroesServerice.getSugerencia( this.termino )
+      .subscribe( heroes => {this.heroes = heroes; this.guardaTermino = this.termino});
+  }
+
+  opcionSeleccionada( event: MatAutocompleteSelectedEvent ) {
+   
+    if( event.option.value){
+       const heroe: Heroe = event.option.value;
+       this.termino = heroe.superhero;
+
+       this.heroesServerice.getHeroesPorId( heroe.id! )
+        .subscribe( heroe => this.heroeSeleccionado = heroe );
+
+        this.sinResultados = false;
+    }
+    else{
+      this.sinResultados = true;
+    }
+    
+  } */
 
 }
--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,16 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, map, switchMap } from 'rxjs';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: [
+    `
+      img{
+        width: 100%;
+        border-radius: 5px;
+      } 
+    `
+
   ]
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe!: Heroe;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroeService: HeroesService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.route.params  
+      .pipe(
+        switchMap( ({ id }) => this.heroeService.getHeroesPorId(id) ) 
+      )
+      .subscribe( heroe => this.heroe = heroe );
   }
+
+  regresar(){
+    this.router.navigate(['/heroes/listado']);
+  }
+
+
+/*
+  YO HABIA HECHO ESTO PERO CON EL SWICTHMAP SE VE MAS BONITO Y ES MEJOR PRACTICA ENTIENDO
+  ngOnInit(): void {
+    this.route.params  
+      .subscribe( 
+        ({id}) => 
+            this.heroeService.getHeroesPorId(id)
+              .subscribe( (heroe) => this.heroe = heroe) );
+  }
+
+
+*/
 
 }
